32543a4b3ff23bc15fd8af4736a7e0cbc2b31ae4
Philippe Luchansky <philuchansky@gmail.com>
2018-03-13 20:09:11 -0700

    add directions to index.js and index.html

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/public/index.html`)
 })
 
+// The back-end
+// 1. Build your Todo model in './models/Todo.js'
+// 2. Build your 5 Todo CRUD routes here.
+//    the "update" route should toggle a Todo's 'completed' Boolean
+// 3. Test all of your routes in postman (they only need to send and receive JSON)
+
 app.listen(PORT, (err) => {
   console.log(err || `Server running on port ${PORT}.`)
 })
