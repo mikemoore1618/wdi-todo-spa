2a9d9f5763911b90e551b59741035ed689fd7983
Philippe Luchansky <philuchansky@gmail.com>
2018-03-13 19:39:45 -0700

    add middleware and mongoose connection

--- index.js
+++ index.js
@@ -6,6 +6,13 @@ const
   bodyParser = require('body-parser')
   PORT = 3000
 
+mongoose.connect('mongodb://localhost/todo-spa', (err) => {
+  console.log(err || "Connected to MongoDB.")
+})
+
+app.use(logger('dev'))
+app.use(bodyParser.json())
+
 app.listen(PORT, (err) => {
   console.log(err || `Server running on port ${PORT}.`)
 })
