d45203ba6b45b95ffc980f9e3d1ff12529356779
Philippe Luchansky <philuchansky@gmail.com>
2018-06-27 10:18:26 -0700

    update documentation comments in Todo model and index.html

--- models/Todo.js
+++ models/Todo.js
@@ -1,4 +1,4 @@
 // the Todo schema should contain the following fields:
 // 1. body, which is a string
 // 2. completed, which is a boolean that defaults to false
-// in addition, the todo schema should also store timestamps for createdAt and updatedAt
+// in addition: the todo schema should also store timestamps for createdAt and updatedAt
